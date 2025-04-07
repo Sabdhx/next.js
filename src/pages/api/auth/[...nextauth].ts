cb14f1c1eb6f2506f1122ed75905d9afc29f22ac
Sabdhx <syedaliabdullahshah5@gmail.com>
2025-04-07 22:59:03 +0500

    changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,11 @@ export const authOptions = {
     GithubProvider({
       clientId:"Ov23limfSI7V32mm4oF2",
       clientSecret:"55aae59a3a2affc04151bce6cda5818e8fcb48ea"
-})]}
+},
+
+)],
+secret: "adee57dae1bae1e3d8626f2ef0937ac1",
+}
 
 
  export default NextAuth(authOptions)
